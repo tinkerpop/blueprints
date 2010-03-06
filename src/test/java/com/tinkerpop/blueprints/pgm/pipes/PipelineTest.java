6b4d1465033c22c7d4129b36347e0c82db5b7431
Marko Rodriguez <okrammarko@gmail.com>
2010-03-05 17:42:35 -0800

    added a property pipe and a updated listmake to collection maker. more test cases---blueprints pipes is nice.

--- src/test/java/com/tinkerpop/blueprints/pgm/pipes/PipelineTest.java
+++ src/test/java/com/tinkerpop/blueprints/pgm/pipes/PipelineTest.java
@@ -13,16 +13,21 @@ import java.util.Arrays;
  */
 public class PipelineTest extends TestCase {
 
-     public void testOneStagePipeline() {
+    public void testOneStagePipeline() {
         Graph graph = TinkerGraphFactory.createTinkerGraph();
         Vertex marko = graph.getVertex("1");
         Pipe vep = new VertexEdgePipe(VertexEdgePipe.Step.OUT_EDGES);
-        Pipe<Vertex, Edge> pipeline = new Pipeline<Vertex,Edge>(Arrays.asList(vep));
+        Pipe<Vertex, Edge> pipeline = new Pipeline<Vertex, Edge>(Arrays.asList(vep));
         pipeline.setStarts(Arrays.asList(marko).iterator());
         assertTrue(pipeline.hasNext());
-        while(pipeline.hasNext()) {
-            System.out.println(pipeline.next());
+        int counter = 0;
+        while (pipeline.hasNext()) {
+            Edge e = pipeline.next();
+            assertTrue(e.getInVertex().getId().equals("4") || e.getInVertex().getId().equals("2") || e.getInVertex().getId().equals("3"));
+            counter++;
         }
+        assertEquals(counter, 3);
+
 
     }
 
@@ -32,12 +37,55 @@ public class PipelineTest extends TestCase {
         Pipe vep = new VertexEdgePipe(VertexEdgePipe.Step.OUT_EDGES);
         Pipe lfp = new LabelFilterPipe(Arrays.asList("created"), false);
         Pipe evp = new EdgeVertexPipe(EdgeVertexPipe.Step.IN_VERTEX);
-        Pipe<Vertex,Vertex> pipeline = new Pipeline<Vertex,Vertex>(Arrays.asList(vep, lfp, evp));
+        Pipe<Vertex, Vertex> pipeline = new Pipeline<Vertex, Vertex>(Arrays.asList(vep, lfp, evp));
         pipeline.setStarts(Arrays.asList(marko).iterator());
         assertTrue(pipeline.hasNext());
-        while(pipeline.hasNext()) {
-            System.out.println(pipeline.next());
+        int counter = 0;
+        while (pipeline.hasNext()) {
+            assertEquals(pipeline.next().getId(), "3");
+            counter++;
         }
+        assertEquals(counter, 1);
+
+        vep = new VertexEdgePipe(VertexEdgePipe.Step.OUT_EDGES);
+        lfp = new LabelFilterPipe(Arrays.asList("created"), true);
+        evp = new EdgeVertexPipe(EdgeVertexPipe.Step.IN_VERTEX);
+        pipeline = new Pipeline<Vertex, Vertex>(Arrays.asList(vep, lfp, evp));
+        pipeline.setStarts(Arrays.asList(marko).iterator());
+        assertTrue(pipeline.hasNext());
+        counter = 0;
+        while (pipeline.hasNext()) {
+            Vertex v = pipeline.next();
+            assertTrue(v.getId().equals("4") || v.getId().equals("2"));
+            counter++;
+        }
+        assertEquals(counter, 2);
 
     }
+
+    public void testPipelineResuse() {
+        Graph graph = TinkerGraphFactory.createTinkerGraph();
+        Vertex marko = graph.getVertex("1");
+        Pipe vep = new VertexEdgePipe(VertexEdgePipe.Step.OUT_EDGES);
+        Pipe evp = new EdgeVertexPipe(EdgeVertexPipe.Step.IN_VERTEX);
+        Pipe<Vertex, Vertex> pipeline = new Pipeline<Vertex, Vertex>(Arrays.asList(vep, evp));
+        pipeline.setStarts(Arrays.asList(marko).iterator());
+        assertTrue(pipeline.hasNext());
+        int counter = 0;
+        if (pipeline.hasNext()) {
+            counter++;
+            pipeline.next();
+        }
+        assertEquals(counter, 1);
+
+        pipeline.setStarts(Arrays.asList(marko).iterator());
+        assertTrue(pipeline.hasNext());
+        counter = 0;
+        while (pipeline.hasNext()) {
+            counter++;
+            pipeline.next();
+        }
+        assertEquals(counter, 3);
+    }
+
 }
---
+++ src/test/java/com/tinkerpop/blueprints/pgm/pipes/PropertyPipeTest.java
@@ -0,0 +1,47 @@
+package com.tinkerpop.blueprints.pgm.pipes;
+
+import com.tinkerpop.blueprints.pgm.Edge;
+import com.tinkerpop.blueprints.pgm.Graph;
+import com.tinkerpop.blueprints.pgm.Vertex;
+import com.tinkerpop.blueprints.pgm.impls.tg.TinkerGraphFactory;
+import junit.framework.TestCase;
+
+import java.util.Arrays;
+
+/**
+ * @author: Marko A. Rodriguez (http://markorodriguez.com)
+ */
+public class PropertyPipeTest extends TestCase {
+
+    public void testSingleProperty() {
+        Graph graph = TinkerGraphFactory.createTinkerGraph();
+        Vertex marko = graph.getVertex("1");
+        PropertyPipe<Vertex,String> pp = new PropertyPipe<Vertex,String>("name");
+        pp.setStarts(Arrays.asList(marko).iterator());
+        assertTrue(pp.hasNext());
+        int counter = 0;
+        while (pp.hasNext()) {
+            String name = pp.next();
+            assertEquals(name, "marko");
+            counter++;
+        }
+        assertEquals(counter, 1);
+    }
+
+    public void testMultiProperty() {
+        Graph graph = TinkerGraphFactory.createTinkerGraph();
+        Vertex marko = graph.getVertex("1");
+        Pipe evp = new EdgeVertexPipe(EdgeVertexPipe.Step.IN_VERTEX);
+        Pipe<Vertex,String> pp = new PropertyPipe<Vertex,String>("name");
+        Pipeline<Edge,String> pipeline = new Pipeline(Arrays.asList(evp, pp));
+        pipeline.setStarts(marko.getOutEdges().iterator());
+        assertTrue(pipeline.hasNext());
+        int counter = 0;
+        while (pipeline.hasNext()) {
+            String name = pipeline.next();
+            assertTrue(name.equals("vadas") || name.equals("josh") || name.equals("lop"));
+            counter++;
+        }
+        assertEquals(counter, 3);
+    }
+}
