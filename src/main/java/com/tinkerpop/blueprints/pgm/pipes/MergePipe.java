74198375d17b5011320e2d7d96d85f52b3164517
Marko Rodriguez <okrammarko@gmail.com>
2010-03-31 13:21:01 -0600

    added mergepipe---allows you to merge the results of one or more pipes into a single pipe. added a couple of test cases and everything works great.

---
+++ src/main/java/com/tinkerpop/blueprints/pgm/pipes/MergePipe.java
@@ -0,0 +1,24 @@
+package com.tinkerpop.blueprints.pgm.pipes;
+
+import java.util.Iterator;
+
+/**
+ * @author: Marko A. Rodriguez (http://markorodriguez.com)
+ */
+public class MergePipe<E> extends AbstractPipe<Iterator<E>, E> {
+
+    protected Iterator<E> nextEnds;
+
+    protected void setNext() {
+        if (null != nextEnds && nextEnds.hasNext()) {
+            this.nextEnd = this.nextEnds.next();
+        } else {
+            if ((null == nextEnds || !this.nextEnds.hasNext()) && this.starts.hasNext()) {
+                this.nextEnds = this.starts.next();
+                this.setNext();
+            } else {
+                this.done = true;
+            }
+        }
+    }
+}
---
+++ src/test/java/com/tinkerpop/blueprints/pgm/pipes/MergePipeTest.java
@@ -0,0 +1,61 @@
+package com.tinkerpop.blueprints.pgm.pipes;
+
+import junit.framework.TestCase;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author: Marko A. Rodriguez (http://markorodriguez.com)
+ */
+public class MergePipeTest extends TestCase {
+
+    public void testMergePipe() {
+        List<String> marko = Arrays.asList("marko", "antonio", "rodriguez");
+        List<String> peter = Arrays.asList("peter", "neubauer");
+        List<String> josh = Arrays.asList("joshua", "shinavier");
+        MergePipe<String> pipe = new MergePipe<String>();
+        pipe.setStarts(Arrays.asList(marko.iterator(), peter.iterator(), josh.iterator()).iterator());
+        assertTrue(pipe.hasNext());
+        int counter = 0;
+        while(pipe.hasNext()) {
+            counter++;
+            String name = pipe.next();
+            System.out.println(name);
+            assertTrue(marko.contains(name) || peter.contains(name) || josh.contains(name));
+        }
+        assertEquals(counter,7);
+    }
+
+    public void testMergePipeEmpties() {
+        List<String> marko = Arrays.asList();
+        List<String> peter = Arrays.asList("peter", "neubauer");
+        List<String> josh = Arrays.asList();
+        MergePipe<String> pipe = new MergePipe<String>();
+        pipe.setStarts(Arrays.asList(marko.iterator(), peter.iterator(), josh.iterator()).iterator());
+        assertTrue(pipe.hasNext());
+        int counter = 0;
+        while(pipe.hasNext()) {
+            counter++;
+            String name = pipe.next();
+            assertTrue(marko.contains(name) || peter.contains(name) || josh.contains(name));
+        }
+        assertEquals(counter,2);
+    }
+
+    public void testMergePipeAllEmpties() {
+        List<String> marko = Arrays.asList();
+        List<String> peter = Arrays.asList();
+        List<String> josh = Arrays.asList();
+        MergePipe<String> pipe = new MergePipe<String>();
+        pipe.setStarts(Arrays.asList(marko.iterator(), peter.iterator(), josh.iterator()).iterator());
+        assertFalse(pipe.hasNext());
+        int counter = 0;
+        while(pipe.hasNext()) {
+            counter++;
+            pipe.next();
+        }
+        assertEquals(counter,0);
+    }
+
+}
