304b46a437f8ad486ed74b985671c1ad8e380194
lvca <luca@192.168.0.10>
2010-09-14 19:22:20 +0200

    Implemented TransactionalGraph interface and improved performances

--- src/main/java/com/tinkerpop/blueprints/pgm/impls/orientdb/OrientElement.java
+++ src/main/java/com/tinkerpop/blueprints/pgm/impls/orientdb/OrientElement.java
@@ -1,95 +1,138 @@
 package com.tinkerpop.blueprints.pgm.impls.orientdb;
 
+import java.util.Set;
+
 import com.orientechnologies.orient.core.db.graph.OGraphElement;
 import com.orientechnologies.orient.core.id.ORID;
 import com.tinkerpop.blueprints.pgm.Element;
-
-import java.util.Set;
 
 /**
  * @author Luca Garulli (http://www.orientechnologies.com)
  */
 public abstract class OrientElement implements Element {
 
-    protected static final String LABEL = "label";
-    protected final OrientGraph graph;
-    protected final OGraphElement raw;
+  protected static final String LABEL = "label";
+  protected final OrientGraph   graph;
+  protected final OGraphElement raw;
 
-    protected OrientElement(final OrientGraph iGraph, final OGraphElement element) {
-        this.graph = iGraph;
-        this.raw = element;
+  protected OrientElement(final OrientGraph iGraph, final OGraphElement iElement) {
+    this.graph = iGraph;
+    this.raw = iElement;
+
+    iGraph.putElementInCache(iElement.getDocument().getIdentity(), this);
+  }
+
+  public void setProperty(final String iKey, final Object value) {
+    final Object oldValue = raw.get(iKey);
+
+    graph.beginTransaction();
+
+    try {
+      this.raw.set(iKey, value);
+      this.save();
+
+      if (oldValue != null)
+        // REMOVE OLD INDEXED PROPERTY
+        graph.getIndex().remove(iKey, oldValue, this);
+      graph.getIndex().put(iKey, value, this);
+      graph.commitTransaction();
+
+    } catch (RuntimeException e) {
+
+      graph.rollbackTransaction();
+      throw e;
     }
+  }
 
-    public void setProperty(final String iKey, final Object value) {
-        final Object oldValue = raw.get(iKey);
+  public Object removeProperty(final String iKey) {
+    graph.beginTransaction();
 
-        this.raw.set(iKey, value);
-        this.save();
+    try {
+      final Object old = this.raw.remove(iKey);
+      this.save();
+      graph.getIndex().remove(iKey, old, this);
+      graph.commitTransaction();
 
-        if (oldValue != null)
-            // REMOVE OLD INDEXED PROPERTY
-            graph.getIndex().remove(iKey, oldValue, this);
-        graph.getIndex().put(iKey, value, this);
+      return old;
+    } catch (RuntimeException e) {
+
+      graph.rollbackTransaction();
+      throw e;
     }
+  }
 
-    public Object removeProperty(final String iKey) {
-        final Object old = this.raw.remove(iKey);
-        this.save();
-        graph.getIndex().remove(iKey, old, this);
-        return old;
-    }
+  public Object getProperty(final String key) {
+    return this.raw.get(key);
+  }
 
-    public Object getProperty(final String key) {
-        return this.raw.get(key);
-    }
+  public Set<String> getPropertyKeys() {
+    final Set<String> set = this.raw.propertyNames();
+    set.remove(LABEL);
+    return set;
+  }
 
-    public Set<String> getPropertyKeys() {
-        final Set<String> set = this.raw.propertyNames();
-        set.remove(LABEL);
-        return set;
-    }
+  /**
+   * Returns the Element Id assuring to save it if it's transient yet.
+   */
+  public Object getId() {
+    ORID rid = this.raw.getId();
+    this.save();
+    return rid;
+  }
 
-    /**
-     * Returns the Element Id assuring to save it if it's transient yet.
-     */
-    public Object getId() {
-        ORID rid = this.raw.getId();
-        this.save();
-        return rid;
-    }
+  protected void delete() {
+    graph.beginTransaction();
 
-    protected void delete() {
-        this.raw.delete();
-    }
+    try {
+      graph.removeElementFromCache(this.raw.getId());
+      this.raw.delete();
+      graph.commitTransaction();
 
-    protected void save() {
-        this.raw.save();
-    }
+    } catch (RuntimeException e) {
 
-    public OGraphElement getRaw() {
-        return raw;
+      graph.rollbackTransaction();
+      throw e;
     }
+  }
 
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((this.raw == null) ? 0 : this.raw.hashCode());
-        return result;
-    }
+  protected void save() {
+    graph.beginTransaction();
 
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        OrientElement other = (OrientElement) obj;
-        if (this.raw == null) {
-            if (other.raw != null)
-                return false;
-        } else if (!this.raw.equals(other.raw))
-            return false;
-        return true;
+    try {
+      this.raw.save();
+      graph.commitTransaction();
+
+    } catch (RuntimeException e) {
+
+      graph.rollbackTransaction();
+      throw e;
     }
+  }
+
+  public OGraphElement getRaw() {
+    return raw;
+  }
+
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((this.raw == null) ? 0 : this.raw.hashCode());
+    return result;
+  }
+
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    final OrientElement other = (OrientElement) obj;
+    if (this.raw == null) {
+      if (other.raw != null)
+        return false;
+    } else if (!this.raw.equals(other.raw))
+      return false;
+    return true;
+  }
 }
--- src/main/java/com/tinkerpop/blueprints/pgm/impls/orientdb/OrientGraph.java
+++ src/main/java/com/tinkerpop/blueprints/pgm/impls/orientdb/OrientGraph.java
@@ -1,8 +1,11 @@
 package com.tinkerpop.blueprints.pgm.impls.orientdb;
 
+import java.util.HashMap;
+
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
 import com.orientechnologies.orient.core.db.graph.ODatabaseGraphTx;
 import com.orientechnologies.orient.core.db.graph.OGraphEdge;
+import com.orientechnologies.orient.core.db.graph.OGraphElement;
 import com.orientechnologies.orient.core.db.graph.OGraphVertex;
 import com.orientechnologies.orient.core.id.ORID;
 import com.orientechnologies.orient.core.id.ORecordId;
@@ -12,126 +15,266 @@ import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.tinkerpop.blueprints.pgm.Edge;
 import com.tinkerpop.blueprints.pgm.Graph;
 import com.tinkerpop.blueprints.pgm.Index;
+import com.tinkerpop.blueprints.pgm.TransactionalGraph;
 import com.tinkerpop.blueprints.pgm.Vertex;
 import com.tinkerpop.blueprints.pgm.impls.orientdb.util.OrientElementSequence;
 
 /**
+ * OrientDB implementation of Graph interface. This implementation is transactional.
+ * 
  * @author Luca Garulli (http://www.orientechnologies.com)
  */
-public class OrientGraph implements Graph {
-    private final ODatabaseGraphTx database;
-    private OrientIndex index;
+public class OrientGraph implements Graph, TransactionalGraph {
+  private final ODatabaseGraphTx       database;
+  private OrientIndex                  index;
+  private HashMap<ORID, OrientElement> rid2Elements = new HashMap<ORID, OrientElement>();
+  private Mode                         mode         = Mode.MANUAL;
+  private boolean                      useCache     = true;
 
-    public OrientGraph(final String url) {
-        this.database = new ODatabaseGraphTx(url);
+  public OrientGraph(final String url) {
+    this.database = new ODatabaseGraphTx(url);
+  }
+
+  public OrientGraph open(final String username, final String password) {
+    this.database.open(username, password);
+    this.index = new OrientIndex(this);
+    rid2Elements.clear();
+    return this;
+  }
+
+  public OrientGraph create() {
+    this.database.create();
+    this.index = new OrientIndex(this);
+    rid2Elements.clear();
+    return this;
+  }
+
+  public Vertex addVertex(final Object id) {
+    try {
+      beginTransaction();
+
+      final OrientVertex vertex = new OrientVertex(this, this.database.createVertex());
+      vertex.save();
+      putElementInCache(vertex.getRaw().getDocument().getIdentity(), vertex);
+
+      commitTransaction();
+      return vertex;
+
+    } catch (RuntimeException e) {
+      rollbackTransaction();
+      throw e;
+    }
+  }
+
+  public Edge addEdge(final Object id, final Vertex outVertex, final Vertex inVertex, final String label) {
+    try {
+      beginTransaction();
+
+      final OrientEdge edge = new OrientEdge(this, ((OrientVertex) outVertex).getRawVertex().link(
+          ((OrientVertex) inVertex).getRawVertex()));
+      edge.setLabel(label);
+
+      ((OrientVertex) outVertex).getRawVertex().getDocument().setDirty();
+      ((OrientVertex) inVertex).getRawVertex().getDocument().setDirty();
+
+      edge.save();
+      putElementInCache(edge.getRaw().getDocument().getIdentity(), edge);
+
+      commitTransaction();
+
+      return edge;
+
+    } catch (RuntimeException e) {
+      rollbackTransaction();
+      throw e;
+    }
+  }
+
+  public Vertex getVertex(final Object id) {
+    ORID rid;
+    if (id instanceof ORID)
+      rid = (ORID) id;
+    else
+      rid = new ORecordId(id.toString());
+
+    // TRY IN CACHE
+    final OrientVertex v = (OrientVertex) getCachedElement(rid);
+    if (v != null)
+      return v;
+
+    final ODocument doc = this.database.getRecordById(rid);
+    if (doc != null) {
+      return new OrientVertex(this, (OGraphVertex) this.database.getUserObjectByRecord(doc, null));
+    } else
+      return new OrientVertex(this, (OGraphVertex) this.database.load(rid));
+  }
+
+  public void removeVertex(final Vertex vertex) {
+    try {
+      beginTransaction();
+
+      final OGraphElement e = ((OrientVertex) vertex).getRaw();
+      removeElementFromCache(e.getId());
+      ((OrientVertex) vertex).delete();
+
+      commitTransaction();
+
+    } catch (RuntimeException e) {
+      rollbackTransaction();
+      throw e;
+    }
+  }
+
+  public Iterable<Vertex> getVertices() {
+    return new OrientElementSequence<Vertex>(this, new OGraphVertexIterator(this.database));
+  }
+
+  public Iterable<Edge> getEdges() {
+    return new OrientElementSequence<Edge>(this, new OGraphEdgeIterator(this.database));
+  }
+
+  public Edge getEdge(final Object id) {
+    final ORID rid;
+    if (id instanceof ORID)
+      rid = (ORID) id;
+    else
+      rid = new ORecordId(id.toString());
+
+    // TRY IN CACHE
+    final OrientEdge e = (OrientEdge) getCachedElement(rid);
+    if (e != null)
+      return e;
+
+    final ODocument doc = this.database.getRecordById(rid);
+    if (doc != null) {
+      return new OrientEdge(this, (OGraphEdge) this.database.getUserObjectByRecord(doc, null));
+    } else
+      return new OrientEdge(this, (OGraphEdge) this.database.load(rid));
+  }
+
+  public void removeEdge(final Edge edge) {
+    try {
+      beginTransaction();
+
+      final OGraphEdge e = (OGraphEdge) ((OrientEdge) edge).getRaw();
+      removeElementFromCache(e.getId());
+
+      // REMOVE FROM CACHE ALSO THE CONNECTED VERTICES
+      removeElementFromCache(e.getIn().getId());
+      removeElementFromCache(e.getOut().getId());
+
+      ((OrientEdge) edge).delete();
+
+      commitTransaction();
+
+    } catch (RuntimeException e) {
+      rollbackTransaction();
+      throw e;
+    }
+  }
+
+  public void clear() {
+    for (ODocument v : ((ODatabaseDocumentTx) this.database.getUnderlying()).browseClass(OGraphVertex.class.getSimpleName())) {
+      if (v != null)
+        v.delete();
     }
 
-    public OrientGraph open(final String username, final String password) {
-        this.database.open(username, password);
-        this.index = new OrientIndex(this);
-        return this;
+    for (ODocument e : ((ODatabaseDocumentTx) this.database.getUnderlying()).browseClass(OGraphEdge.class.getSimpleName())) {
+      if (e != null)
+        e.delete();
     }
 
-    public OrientGraph create() {
-        this.database.create();
-        this.index = new OrientIndex(this);
-        return this;
-    }
+    index.clear();
+    rid2Elements.clear();
+  }
 
-    public Vertex addVertex(final Object id) {
-        final OrientVertex vertex = new OrientVertex(this, this.database.createVertex());
-        vertex.save();
-        return vertex;
-    }
+  public Index getIndex() {
+    return index;
+  }
 
-    public Edge addEdge(final Object id, final Vertex outVertex, final Vertex inVertex, final String label) {
-        final OrientEdge edge = new OrientEdge(this, ((OrientVertex) outVertex).getRawVertex().link(
-                ((OrientVertex) inVertex).getRawVertex()));
-        edge.setLabel(label);
+  public void shutdown() {
+    this.database.close();
+    this.index = null;
+    rid2Elements.clear();
+  }
 
-        ((OrientVertex) outVertex).getRawVertex().getDocument().setDirty();
-        ((OrientVertex) inVertex).getRawVertex().getDocument().setDirty();
+  public boolean exists() {
+    return this.database.exists();
+  }
 
-        edge.save();
-        return edge;
-    }
+  public String toString() {
+    return "orientgraph[" + this.database.getURL() + "]";
+  }
 
-    public Vertex getVertex(final Object id) {
-        ORID rid;
-        if (id instanceof ORID)
-            rid = (ORID) id;
-        else
-            rid = new ORecordId(id.toString());
+  public OrientElement getCachedElement(final ORID iRID) {
+    if (useCache && iRID.isValid())
+      return rid2Elements.get(iRID);
+    return null;
+  }
 
-        final ODocument doc = this.database.getRecordById(rid);
-        if (doc != null) {
-            return new OrientVertex(this, (OGraphVertex) this.database.getUserObjectByRecord(doc, null));
-        } else
-            return new OrientVertex(this, (OGraphVertex) this.database.load(rid));
-    }
+  public void putElementInCache(final ORID iRID, final OrientElement iElement) {
+    if (useCache && iRID.isValid())
+      rid2Elements.put(iRID, iElement);
+  }
 
-    public void removeVertex(final Vertex vertex) {
-        ((OrientVertex) vertex).getRawVertex().delete();
-    }
+  public void removeElementFromCache(final ORID iRID) {
+    if (useCache && iRID.isValid())
+      rid2Elements.remove(iRID);
+  }
 
-    public Iterable<Vertex> getVertices() {
-        return new OrientElementSequence<Vertex>(this, new OGraphVertexIterator(this.database));
-    }
+  public ODatabaseGraphTx getRawGraph() {
+    return database;
+  }
 
-    public Iterable<Edge> getEdges() {
-        return new OrientElementSequence<Edge>(this, new OGraphEdgeIterator(this.database));
-    }
+  @Override
+  public void startTransaction() {
+    if (Mode.AUTOMATIC == this.mode)
+      throw new RuntimeException(TransactionalGraph.TURN_OFF_MESSAGE);
 
-    public Edge getEdge(final Object id) {
-        final ORID rid;
-        if (id instanceof ORID)
-            rid = (ORID) id;
-        else
-            rid = new ORecordId(id.toString());
+    database.begin();
+  }
 
-        final ODocument doc = this.database.getRecordById(rid);
-        if (doc != null) {
-            return new OrientEdge(this, (OGraphEdge) this.database.getUserObjectByRecord(doc, null));
-        } else
-            return new OrientEdge(this, (OGraphEdge) this.database.load(rid));
-    }
+  @Override
+  public void stopTransaction(final Conclusion conclusion) {
+    if (Mode.AUTOMATIC == this.mode)
+      throw new RuntimeException(TransactionalGraph.TURN_OFF_MESSAGE);
 
-    public void removeEdge(final Edge edge) {
-        ((OrientEdge) edge).delete();
-    }
+    if (conclusion == Conclusion.FAILURE)
+      database.rollback();
+    else
+      database.commit();
+  }
 
-    public void clear() {
-        for (ODocument v : ((ODatabaseDocumentTx) this.database.getUnderlying()).browseClass(OGraphVertex.class.getSimpleName())) {
-            if (v != null)
-                v.delete();
-        }
+  @Override
+  public void setTransactionMode(final Mode iMode) {
+    mode = iMode;
+  }
 
-        for (ODocument e : ((ODatabaseDocumentTx) this.database.getUnderlying()).browseClass(OGraphEdge.class.getSimpleName())) {
-            if (e != null)
-                e.delete();
-        }
+  @Override
+  public Mode getTransactionMode() {
+    return mode;
+  }
 
-        index.clear();
-    }
+  public void beginTransaction() {
+    if (getTransactionMode() == Mode.AUTOMATIC)
+      database.begin();
+  }
 
-    public Index getIndex() {
-        return index;
-    }
+  public void commitTransaction() {
+    if (getTransactionMode() == Mode.AUTOMATIC)
+      database.commit();
+  }
 
-    public void shutdown() {
-        this.database.close();
-        this.index = null;
-    }
+  public void rollbackTransaction() {
+    if (getTransactionMode() == Mode.AUTOMATIC)
+      database.rollback();
+  }
 
-    public boolean exists() {
-        return this.database.exists();
-    }
+  public boolean isUseCache() {
+    return useCache;
+  }
 
-    public String toString() {
-        return "orientgraph[" + this.database.getURL() + "]";
-    }
-
-    public ODatabaseGraphTx getRawGraph() {
-        return database;
-    }
+  public void setUseCache(boolean useCache) {
+    this.useCache = useCache;
+  }
 }
