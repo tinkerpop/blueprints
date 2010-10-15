703f28b1c6c7e12f39dce38a90577192ee344046
lvca <luca@PC-luca>
2010-10-15 20:49:31 +0200

    Fixed issue of last Test Cases about loading of graphs from file in separate transactions

--- src/main/java/com/tinkerpop/blueprints/pgm/impls/orientdb/OrientGraph.java
+++ src/main/java/com/tinkerpop/blueprints/pgm/impls/orientdb/OrientGraph.java
@@ -1,6 +1,5 @@
 package com.tinkerpop.blueprints.pgm.impls.orientdb;
 
-import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
 import com.orientechnologies.orient.core.db.graph.ODatabaseGraphTx;
 import com.orientechnologies.orient.core.db.graph.OGraphEdge;
 import com.orientechnologies.orient.core.db.graph.OGraphVertex;
@@ -17,210 +16,228 @@ import com.tinkerpop.blueprints.pgm.impls.orientdb.util.OrientElementSequence;
 
 /**
  * OrientDB implementation of Graph interface. This implementation is transactional.
- *
+ * 
  * @author Luca Garulli (http://www.orientechnologies.com)
  */
 public class OrientGraph implements Graph, TransactionalGraph {
-    private final ODatabaseGraphTx database;
-    private OrientIndex index;
-    private Mode mode = Mode.AUTOMATIC;
-    private final static String ADMIN = "admin";
+	private ODatabaseGraphTx		database;
+	private OrientIndex					index;
 
-    public OrientGraph(final String url) {
-        this(url, ADMIN, ADMIN);
-    }
+	private final String				url;
+	private final String				username;
+	private final String				password;
 
-    @SuppressWarnings("serial")
-    public OrientGraph(final String url, final String username, final String password) {
-        this.database = new ODatabaseGraphTx(url);
-        if (this.database.exists())
-            this.database.open(username, password);
-        else
-            this.database.create();
+	private Mode								mode	= Mode.AUTOMATIC;
+	private final static String	ADMIN	= "admin";
 
-        this.index = new OrientIndex(this);
-    }
+	public OrientGraph(final String url) {
+		this(url, ADMIN, ADMIN);
+	}
 
-    public Vertex addVertex(final Object id) {
-        try {
-            beginTransaction();
+	public OrientGraph(final String url, final String username, final String password) {
+		this.url = url;
+		this.username = username;
+		this.password = password;
+		openOrCreate();
+	}
 
-            final OrientVertex vertex = new OrientVertex(this, this.database.createVertex(null));
-            vertex.save();
+	public Vertex addVertex(final Object id) {
+		try {
+			beginTransaction();
 
-            commitTransaction();
-            return vertex;
+			final OrientVertex vertex = new OrientVertex(this, this.database.createVertex(null));
+			vertex.save();
 
-        } catch (RuntimeException e) {
-            rollbackTransaction();
-            throw e;
-        }
-    }
+			commitTransaction();
+			return vertex;
 
-    public Edge addEdge(final Object id, final Vertex outVertex, final Vertex inVertex, final String label) {
-        try {
-            beginTransaction();
+		} catch (RuntimeException e) {
+			rollbackTransaction();
+			throw e;
+		}
+	}
 
-            final OrientEdge edge = new OrientEdge(this, ((OrientVertex) outVertex).getRawVertex().link(
-                    ((OrientVertex) inVertex).getRawVertex()));
-            edge.setLabel(label);
+	public Edge addEdge(final Object id, final Vertex outVertex, final Vertex inVertex, final String label) {
+		try {
+			beginTransaction();
 
-            ((OrientVertex) outVertex).getRawVertex().getDocument().setDirty();
-            ((OrientVertex) inVertex).getRawVertex().getDocument().setDirty();
+			final OrientEdge edge = new OrientEdge(this, ((OrientVertex) outVertex).getRawVertex().link(
+					((OrientVertex) inVertex).getRawVertex()));
+			edge.setLabel(label);
 
-            // SAVE THE VERTEX TO ASSURE THEY ARE IN TX
-            ((OrientVertex) outVertex).save();
-            ((OrientVertex) inVertex).save();
-            edge.save();
+			((OrientVertex) outVertex).getRawVertex().getDocument().setDirty();
+			((OrientVertex) inVertex).getRawVertex().getDocument().setDirty();
 
-            commitTransaction();
+			// SAVE THE VERTEX TO ASSURE THEY ARE IN TX
+			((OrientVertex) outVertex).save();
+			((OrientVertex) inVertex).save();
+			edge.save();
 
-            return edge;
+			commitTransaction();
 
-        } catch (RuntimeException e) {
-            rollbackTransaction();
-            throw e;
-        }
-    }
+			return edge;
 
-    public Vertex getVertex(final Object id) {
-        ORID rid;
-        if (id instanceof ORID)
-            rid = (ORID) id;
-        else
-            rid = new ORecordId(id.toString());
+		} catch (RuntimeException e) {
+			rollbackTransaction();
+			throw e;
+		}
+	}
 
-        if (!rid.isValid())
-            return null;
+	public Vertex getVertex(final Object id) {
+		ORID rid;
+		if (id instanceof ORID)
+			rid = (ORID) id;
+		else
+			rid = new ORecordId(id.toString());
 
-        final ODocument doc = this.database.getRecordById(rid);
-        if (doc != null) {
-            return new OrientVertex(this, (OGraphVertex) this.database.getUserObjectByRecord(doc, null));
-        } else
-            return new OrientVertex(this, (OGraphVertex) this.database.load(rid));
-    }
+		if (!rid.isValid())
+			return null;
 
-    public void removeVertex(final Vertex vertex) {
-        try {
-            beginTransaction();
-            ((OrientVertex) vertex).delete();
-            commitTransaction();
+		final ODocument doc = this.database.getRecordById(rid);
+		if (doc != null) {
+			return new OrientVertex(this, (OGraphVertex) this.database.getUserObjectByRecord(doc, null));
+		} else
+			return new OrientVertex(this, (OGraphVertex) this.database.load(rid));
+	}
 
-        } catch (RuntimeException e) {
-            rollbackTransaction();
-            throw e;
-        }
-    }
+	public void removeVertex(final Vertex vertex) {
+		try {
+			beginTransaction();
+			((OrientVertex) vertex).delete();
+			commitTransaction();
 
-    public Iterable<Vertex> getVertices() {
-        return new OrientElementSequence<Vertex>(this, new OGraphVertexIterator(this.database));
-    }
+		} catch (RuntimeException e) {
+			rollbackTransaction();
+			throw e;
+		}
+	}
 
-    public Iterable<Edge> getEdges() {
-        return new OrientElementSequence<Edge>(this, new OGraphEdgeIterator(this.database));
-    }
+	public Iterable<Vertex> getVertices() {
+		return new OrientElementSequence<Vertex>(this, new OGraphVertexIterator(this.database));
+	}
 
-    public Edge getEdge(final Object id) {
-        final ORID rid;
-        if (id instanceof ORID)
-            rid = (ORID) id;
-        else
-            rid = new ORecordId(id.toString());
+	public Iterable<Edge> getEdges() {
+		return new OrientElementSequence<Edge>(this, new OGraphEdgeIterator(this.database));
+	}
 
-        final ODocument doc = this.database.getRecordById(rid);
-        if (doc != null) {
-            return new OrientEdge(this, (OGraphEdge) this.database.getUserObjectByRecord(doc, null));
-        } else
-            return new OrientEdge(this, (OGraphEdge) this.database.load(rid));
-    }
+	public Edge getEdge(final Object id) {
+		final ORID rid;
+		if (id instanceof ORID)
+			rid = (ORID) id;
+		else
+			rid = new ORecordId(id.toString());
 
-    public void removeEdge(final Edge edge) {
-        try {
-            beginTransaction();
+		final ODocument doc = this.database.getRecordById(rid);
+		if (doc != null) {
+			return new OrientEdge(this, (OGraphEdge) this.database.getUserObjectByRecord(doc, null));
+		} else
+			return new OrientEdge(this, (OGraphEdge) this.database.load(rid));
+	}
 
-            final OGraphEdge e = (OGraphEdge) ((OrientEdge) edge).getRawElement();
-            ((OrientEdge) edge).delete();
+	public void removeEdge(final Edge edge) {
+		try {
+			beginTransaction();
 
-            commitTransaction();
+			((OrientEdge) edge).delete();
 
-        } catch (RuntimeException e) {
-            rollbackTransaction();
-            throw e;
-        }
-    }
+			commitTransaction();
 
-    public void clear() {
-        for (ODocument v : ((ODatabaseDocumentTx) this.database.getUnderlying()).browseClass(OGraphVertex.class.getSimpleName())) {
-            if (v != null)
-                v.delete();
-        }
+		} catch (RuntimeException e) {
+			rollbackTransaction();
+			throw e;
+		}
+	}
 
-        for (ODocument e : ((ODatabaseDocumentTx) this.database.getUnderlying()).browseClass(OGraphEdge.class.getSimpleName())) {
-            if (e != null)
-                e.delete();
-        }
+	public void clear() {
+		// CLOSE AND DELETE DATABASE
+		this.database.delete();
+		this.database = null;
+		this.index = null;
 
-        this.index.clear();
-    }
+		// REOPEN IT
+		openOrCreate();
+		
+		// for (ODocument v : ((ODatabaseDocumentTx) this.database.getUnderlying()).browseClass(OGraphVertex.class.getSimpleName())) {
+		// if (v != null)
+		// v.delete();
+		// }
+		//
+		// for (ODocument e : ((ODatabaseDocumentTx) this.database.getUnderlying()).browseClass(OGraphEdge.class.getSimpleName())) {
+		// if (e != null)
+		// e.delete();
+		// }
+		//
+		// this.index.clear();
+	}
 
-    public OrientIndex getIndex() {
-        return this.index;
-    }
+	public OrientIndex getIndex() {
+		return this.index;
+	}
 
-    public void shutdown() {
-        this.database.close();
-        this.index = null;
-    }
+	public void shutdown() {
+		this.database.close();
+		this.database = null;
+		this.index = null;
+	}
 
-    public String toString() {
-        return "orientgraph[" + this.database.getURL() + "]";
-    }
+	public String toString() {
+		return "orientgraph[" + this.database.getURL() + "]";
+	}
 
-    public ODatabaseGraphTx getRawGraph() {
-        return database;
-    }
+	public ODatabaseGraphTx getRawGraph() {
+		return database;
+	}
 
-    public void startTransaction() {
-        if (Mode.AUTOMATIC == this.mode)
-            throw new RuntimeException(TransactionalGraph.TURN_OFF_MESSAGE);
+	public void startTransaction() {
+		if (Mode.AUTOMATIC == this.mode)
+			throw new RuntimeException(TransactionalGraph.TURN_OFF_MESSAGE);
 
-        database.begin();
-    }
+		database.begin();
+	}
 
-    public void stopTransaction(final Conclusion conclusion) {
-        if (Mode.AUTOMATIC == this.mode)
-            throw new RuntimeException(TransactionalGraph.TURN_OFF_MESSAGE);
+	public void stopTransaction(final Conclusion conclusion) {
+		if (Mode.AUTOMATIC == this.mode)
+			throw new RuntimeException(TransactionalGraph.TURN_OFF_MESSAGE);
 
-        if (conclusion == Conclusion.FAILURE) {
-            database.rollback();
-            index.getRawIndex().unload();
-        } else
-            database.commit();
-    }
+		if (conclusion == Conclusion.FAILURE) {
+			database.rollback();
+			index.getRawIndex().unload();
+		} else
+			database.commit();
+	}
 
-    public void setTransactionMode(final Mode mode) {
-        this.mode = mode;
-    }
+	public void setTransactionMode(final Mode mode) {
+		this.mode = mode;
+	}
 
-    public Mode getTransactionMode() {
-        return this.mode;
-    }
+	public Mode getTransactionMode() {
+		return this.mode;
+	}
 
-    protected void beginTransaction() {
-        if (getTransactionMode() == Mode.AUTOMATIC)
-            database.begin();
-    }
+	protected void beginTransaction() {
+		if (getTransactionMode() == Mode.AUTOMATIC)
+			database.begin();
+	}
 
-    protected void commitTransaction() {
-        if (getTransactionMode() == Mode.AUTOMATIC)
-            database.commit();
-    }
+	protected void commitTransaction() {
+		if (getTransactionMode() == Mode.AUTOMATIC)
+			database.commit();
+	}
 
-    protected void rollbackTransaction() {
-        if (getTransactionMode() == Mode.AUTOMATIC) {
-            database.rollback();
-            index.getRawIndex().unload();
+	protected void rollbackTransaction() {
+		if (getTransactionMode() == Mode.AUTOMATIC) {
+			database.rollback();
+			index.getRawIndex().unload();
 
-        }
-    }
+		}
+	}
+
+	private void openOrCreate() {
+		this.database = new ODatabaseGraphTx(url);
+		if (this.database.exists())
+			this.database.open(username, password);
+		else
+			this.database.create();
+		this.index = new OrientIndex(this);
+	}
 }
--- src/test/java/com/tinkerpop/blueprints/pgm/impls/orientdb/OrientGraphTest.java
+++ src/test/java/com/tinkerpop/blueprints/pgm/impls/orientdb/OrientGraphTest.java
@@ -9,75 +9,75 @@ import java.lang.reflect.Method;
 
 /**
  * Test suite for OrientDB graph implementation.
- *
+ * 
  * @author Luca Garulli (http://www.orientechnologies.com)
  */
 public class OrientGraphTest extends BaseTest {
 
-    private static final SuiteConfiguration config = new SuiteConfiguration();
+	private static final SuiteConfiguration	config	= new SuiteConfiguration();
 
-    static {
-        config.allowsDuplicateEdges = true;
-        config.allowsSelfLoops = true;
-        config.requiresRDFIds = false;
-        config.isRDFModel = false;
-        config.supportsVertexIteration = true;
-        config.supportsEdgeIteration = true;
-        config.supportsVertexIndex = true;
-        config.supportsEdgeIndex = true;
-        config.ignoresSuppliedIds = true;
-        config.supportsTransactions = true;
-    }
+	static {
+		config.allowsDuplicateEdges = true;
+		config.allowsSelfLoops = true;
+		config.requiresRDFIds = false;
+		config.isRDFModel = false;
+		config.supportsVertexIteration = true;
+		config.supportsEdgeIteration = true;
+		config.supportsVertexIndex = true;
+		config.supportsEdgeIndex = true;
+		config.ignoresSuppliedIds = true;
+		config.supportsTransactions = true;
+	}
 
-    public void testVertexSuite() throws Exception {
-        doSuiteTest(new VertexTestSuite(config));
-    }
+	public void testVertexSuite() throws Exception {
+		doSuiteTest(new VertexTestSuite(config));
+	}
 
-    public void testEdgeSuite() throws Exception {
-        doSuiteTest(new EdgeTestSuite(config));
-    }
+	public void testEdgeSuite() throws Exception {
+		doSuiteTest(new EdgeTestSuite(config));
+	}
 
-    public void testGraphSuite() throws Exception {
-        doSuiteTest(new GraphTestSuite(config));
-    }
+	public void testGraphSuite() throws Exception {
+		doSuiteTest(new GraphTestSuite(config));
+	}
 
-    public void testIndexSuite() throws Exception {
-        doSuiteTest(new IndexTestSuite(config));
-    }
+	public void testIndexSuite() throws Exception {
+		doSuiteTest(new IndexTestSuite(config));
+	}
 
-    /*public void testGraphMLReaderSuite() throws Exception {
-        doSuiteTest(new GraphMLReaderTestSuite(config));
-    }*/
+	public void testGraphMLReaderSuite() throws Exception {
+		doSuiteTest(new GraphMLReaderTestSuite(config));
+	}
 
-    public void testTransactionalGraphTestSuite() throws Exception {
-        doSuiteTest(new TransactionalGraphTestSuite(config));
-    }
+	public void testTransactionalGraphTestSuite() throws Exception {
+		doSuiteTest(new TransactionalGraphTestSuite(config));
+	}
 
-    private void doSuiteTest(final ModelTestSuite suite) throws Exception {
-        String doTest = System.getProperty("testOrientGraph");
-        if (doTest == null || doTest.equals("true")) {
-            String url = System.getProperty("orientGraphDirectory");
-            if (url == null)
-                url = "/tmp/blueprints_test";
+	private void doSuiteTest(final ModelTestSuite suite) throws Exception {
+		String doTest = System.getProperty("testOrientGraph");
+		if (doTest == null || doTest.equals("true")) {
+			String url = System.getProperty("orientGraphDirectory");
+			if (url == null)
+				url = "/tmp/blueprints_test";
 
-            final File directory = new File(url);
-            if (!directory.exists())
-                directory.mkdirs();
+			final File directory = new File(url);
+			if (!directory.exists())
+				directory.mkdirs();
 
-            for (Method method : suite.getClass().getDeclaredMethods()) {
-                if (method.getName().startsWith("test")) {
+			for (Method method : suite.getClass().getDeclaredMethods()) {
+				if (method.getName().startsWith("test")) {
 
-                    System.out.println("Testing " + method.getName() + "...");
+					System.out.println("Testing " + method.getName() + "...");
 
-                    OrientGraph graph = new OrientGraph("local:" + url + "/graph");
-                    graph.clear();
+					OrientGraph graph = new OrientGraph("local:" + url + "/graph");
+					graph.clear();
 
-                    method.invoke(suite, graph);
+					method.invoke(suite, graph);
 
-                    graph.shutdown();
-                    deleteDirectory(new File(url));
-                }
-            }
-        }
-    }
+					graph.shutdown();
+					deleteDirectory(new File(url));
+				}
+			}
+		}
+	}
 }
