34eda84de2148cb25101a82a3eca0da20794f552
okram <okrammarko@gmail.com>
2010-12-01 09:31:38 -0700

    added a CodeExamples class the mirrors the Code Examples wiki in the documentation.

---
+++ src/test/java/com/tinkerpop/blueprints/pgm/impls/tg/CodeExamples.java
@@ -0,0 +1,42 @@
+package com.tinkerpop.blueprints.pgm.impls.tg;
+
+import com.tinkerpop.blueprints.pgm.*;
+import junit.framework.TestCase;
+
+/**
+ * This class maintains a collection of standard Blueprints patterns.
+ * This is used as a tutorial for how to use Blueprints.
+ *
+ * @author Marko A. Rodriguez (http://markorodriguez.com)
+ */
+public class CodeExamples extends TestCase {
+
+    public void testSimpleGraphCreation() {
+        Graph graph = new TinkerGraph();
+        Vertex a = graph.addVertex(null);
+        Vertex b = graph.addVertex(null);
+        a.setProperty("name", "marko");
+        a.setProperty("name", "peter");
+        Edge e = graph.addEdge(null, a, b, "knows");
+        graph.shutdown();
+    }
+
+    public void testIterateThroughEdges() {
+        // creates the graph diagrammed here
+        //  https://github.com/tinkerpop/blueprints/wiki/Property-Graph-Model
+        Graph graph = TinkerGraphFactory.createTinkerGraph();
+        Vertex a = graph.getVertex("1");
+        System.out.println("vertex " + a.getId() + " has name " + a.getProperty("name"));
+        for (Edge e : a.getOutEdges()) {
+            System.out.println(e);
+        }
+    }
+
+    public void testGetAVertexByProperty() {
+        // creates the graph diagrammed here
+        //  https://github.com/tinkerpop/blueprints/wiki/Property-Graph-Model
+        Graph graph = TinkerGraphFactory.createTinkerGraph();
+        Vertex a = ((IndexableGraph)graph).getIndex(Index.VERTICES, Vertex.class).get("name", "marko").iterator().next();
+        System.out.println("vertex " + a.getId() + " has age " + a.getProperty("age"));
+    }
+}
