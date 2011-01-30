ebe1edcd9b7969dcd5f4ae81529202d0b7c6c916
okram <okrammarko@gmail.com>
2011-01-30 10:29:45 -0700

    manually added Luca's updates to OrientGraph for the latest Orient 0.9.25-SNAPSHOT.

--- src/main/java/com/tinkerpop/blueprints/pgm/impls/orientdb/OrientAutomaticIndex.java
+++ src/main/java/com/tinkerpop/blueprints/pgm/impls/orientdb/OrientAutomaticIndex.java
@@ -1,74 +1,70 @@
 package com.tinkerpop.blueprints.pgm.impls.orientdb;
 
-import com.orientechnologies.orient.core.db.record.ORecordTrackedList;
-import com.orientechnologies.orient.core.record.impl.ODocument;
-import com.tinkerpop.blueprints.pgm.AutomaticIndex;
-
 import java.util.HashSet;
 import java.util.Set;
+
+import com.orientechnologies.orient.core.db.record.ORecordTrackedList;
+import com.orientechnologies.orient.core.index.OIndex;
+import com.tinkerpop.blueprints.pgm.AutomaticIndex;
+import com.tinkerpop.blueprints.pgm.Index;
 
 /**
  * @author Marko A. Rodriguez (http://markorodriguez.com)
  */
 public class OrientAutomaticIndex<T extends OrientElement> extends OrientIndex<T> implements AutomaticIndex<T> {
 
-    Set<String> autoIndexKeys;
-    private static final String KEYS = "keys";
+	Set<String>									autoIndexKeys	= null;
+	private static final String	KEYS					= "keys";
 
-    public OrientAutomaticIndex(final String name, final Class<T> indexClass, Set<String> indexKeys, final OrientGraph graph, final ODocument indexCfg) {
-        super(name, indexClass, graph, indexCfg);
-        if (null == indexKeys)
-            this.autoIndexKeys = null;
-        else {
-            this.autoIndexKeys = new HashSet<String>();
-            this.autoIndexKeys.addAll(indexKeys);
-        }
-        this.init();
-        this.saveConfiguration();
+	public OrientAutomaticIndex(final OrientGraph iGraph, final String indexName, final Class<T> iIndexClass, Set<String> indexKeys) {
+		super(iGraph, indexName, iIndexClass, Index.Type.AUTOMATIC);
+		if (indexKeys != null)
+			autoIndexKeys = new HashSet<String>(indexKeys);
+		init();
+		saveConfiguration();
+	}
 
-    }
+	public OrientAutomaticIndex(OrientGraph iGraph, OIndex iIndex) {
+		super(iGraph, iIndex);
+		init();
+	}
 
-    public OrientAutomaticIndex(final String name, final OrientGraph graph, final ODocument indexCfg) {
-        super(name, null, graph, indexCfg);
-        this.init();
+	public Type getIndexType() {
+		return Type.AUTOMATIC;
+	}
 
-    }
+	protected void autoUpdate(final String key, final Object newValue, final Object oldValue, final T element) {
+		if (this.getIndexClass().isAssignableFrom(element.getClass())
+				&& (this.autoIndexKeys == null || this.autoIndexKeys.contains(key))) {
+			if (oldValue != null)
+				this.remove(key, oldValue, element);
+			this.put(key, newValue, element);
+		}
+	}
 
-    public Type getIndexType() {
-        return Type.AUTOMATIC;
-    }
+	protected void autoRemove(final String key, final Object oldValue, final T element) {
+		if (this.getIndexClass().isAssignableFrom(element.getClass())
+				&& (this.autoIndexKeys == null || this.autoIndexKeys.contains(key))) {
+			this.remove(key, oldValue, element);
+		}
+	}
 
+	public Set<String> getAutoIndexKeys() {
+		return this.autoIndexKeys;
+	}
 
-    protected void autoUpdate(final String key, final Object newValue, final Object oldValue, final T element) {
-        if (this.getIndexClass().isAssignableFrom(element.getClass()) && (this.autoIndexKeys == null || this.autoIndexKeys.contains(key))) {
-            if (oldValue != null)
-                this.remove(key, oldValue, element);
-            this.put(key, newValue, element);
-        }
-    }
+	private void init() {
+		ORecordTrackedList field = underlying.getConfiguration().field(KEYS);
+		if (null != field) {
+			this.autoIndexKeys = new HashSet<String>();
+			for (Object key : field) {
+				this.autoIndexKeys.add((String) key);
+			}
+		}
+	}
 
-    protected void autoRemove(final String key, final Object oldValue, final T element) {
-        if (this.getIndexClass().isAssignableFrom(element.getClass()) && (this.autoIndexKeys == null || this.autoIndexKeys.contains(key))) {
-            this.remove(key, oldValue, element);
-        }
-    }
-
-    public Set<String> getAutoIndexKeys() {
-        return this.autoIndexKeys;
-    }
-
-    private void init() {
-        ORecordTrackedList field = indexCfg.field(KEYS);
-        if (null != field) {
-            this.autoIndexKeys = new HashSet<String>();
-            for (Object key : field) {
-                this.autoIndexKeys.add((String) key);
-            }
-        }
-    }
-
-    private void saveConfiguration() {
-        indexCfg.field(KEYS, this.autoIndexKeys);
-        graph.saveIndexConfiguration();
-    }
+	private void saveConfiguration() {
+		underlying.getConfiguration().field(KEYS, this.autoIndexKeys);
+		graph.saveIndexConfiguration();
+	}
 }
