e356bc5608ab6c08a4d166813c7ff1fdc817dcda
okram <okrammarko@gmail.com>
2011-02-08 15:48:57 -0700

    renamed to CommitManager.

--- src/main/java/com/tinkerpop/blueprints/pgm/util/TransactionalGraphHelper.java
+++ src/main/java/com/tinkerpop/blueprints/pgm/util/TransactionalGraphHelper.java
@@ -8,25 +8,25 @@ import com.tinkerpop.blueprints.pgm.TransactionalGraph;
 public class TransactionalGraphHelper {
 
     /**
-     * An OperationCounter puts the transactional graph into manual transaction mode.
+     * A CommitManager puts the transactional graph into manual transaction mode.
      * Next, a transaction is started. When the internal counter has reached the provided buffer size, the transaction is committed.
-     * Upon closing the OperationCounter, the open transaction is committed and the graph is put back into its original transaction mode.
+     * Upon closing the CommitManager, the open transaction is committed and the graph is put back into its original transaction mode.
      *
      * @param graph      the transactional graph to maintain a counter for
      * @param bufferSize the number of counters before the transaction is committed
-     * @return the OperationCounter to use when manipulating the transactional graph
+     * @return the CommitManager to use when manipulating the transactional graph
      */
-    public static OperationCounter createOperationCounter(final TransactionalGraph graph, final int bufferSize) {
-        return new OperationCounter(graph, bufferSize);
+    public static CommitManager createCommitManager(final TransactionalGraph graph, final int bufferSize) {
+        return new CommitManager(graph, bufferSize);
     }
 
-    public static class OperationCounter {
+    public static class CommitManager {
         private final TransactionalGraph graph;
         private final TransactionalGraph.Mode startMode;
         private final int bufferSize;
         private long counter = 0;
 
-        public OperationCounter(final TransactionalGraph graph, final int bufferSize) {
+        public CommitManager(final TransactionalGraph graph, final int bufferSize) {
             this.graph = graph;
             this.startMode = graph.getTransactionMode();
             this.bufferSize = bufferSize;
@@ -61,14 +61,14 @@ public class TransactionalGraphHelper {
          * This is useful for printing information to the screen when parsing large data sets.
          * For example: if(counter.atCommit()) { System.out.print('.'); }
          *
-         * @return whether the operation counter committed a transaction on the last incrCounter()
+         * @return whether the CommitManager committed a transaction on the last incrCounter()
          */
         public boolean atCommit() {
             return (this.counter % this.bufferSize) == 0;
         }
 
         /**
-         * Close the OperationCounter by committing the current transaction.
+         * Close the CommitManager by committing the current transaction.
          * Finally, set the transactional graph to its original transaction mode.
          */
         public void close() {
