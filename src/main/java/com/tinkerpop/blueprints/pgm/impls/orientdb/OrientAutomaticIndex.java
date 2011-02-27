91cdb6f39d084c2ea5760c06c2cda876f83be14a
Luca Garulli <l.garulli@gmail.com>
2011-02-26 18:22:03 -0800

    Edited src/main/java/com/tinkerpop/blueprints/pgm/impls/orientdb/OrientAutomaticIndex.java via GitHub

--- src/main/java/com/tinkerpop/blueprints/pgm/impls/orientdb/OrientAutomaticIndex.java
+++ src/main/java/com/tinkerpop/blueprints/pgm/impls/orientdb/OrientAutomaticIndex.java
@@ -1,58 +1,73 @@
-package com.tinkerpop.blueprints.pgm.impls.orientdb.util;
+package com.tinkerpop.blueprints.pgm.impls.orientdb;
 
-import com.orientechnologies.orient.core.record.ORecord.STATUS;
-import com.orientechnologies.orient.core.record.impl.ODocument;
-import com.tinkerpop.blueprints.pgm.Element;
-import com.tinkerpop.blueprints.pgm.impls.orientdb.OrientEdge;
-import com.tinkerpop.blueprints.pgm.impls.orientdb.OrientElement;
-import com.tinkerpop.blueprints.pgm.impls.orientdb.OrientGraph;
-import com.tinkerpop.blueprints.pgm.impls.orientdb.OrientVertex;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
-import java.util.Iterator;
-import java.util.NoSuchElementException;
+import com.orientechnologies.orient.core.index.OIndex;
+import com.tinkerpop.blueprints.pgm.AutomaticIndex;
+import com.tinkerpop.blueprints.pgm.Index;
+import com.tinkerpop.blueprints.pgm.impls.StringFactory;
 
 /**
- * @author Luca Garulli (http://www.orientechnologies.com)
+ * @author Marko A. Rodriguez (http://markorodriguez.com)
  */
-public class OrientElementSequence<T extends Element> implements Iterator<T>, Iterable<T> {
-    private final Iterator<?> underlying;
-    private final OrientGraph graph;
+public class OrientAutomaticIndex<T extends OrientElement> extends OrientIndex<T> implements AutomaticIndex<T> {
 
-    public OrientElementSequence(final OrientGraph graph, final Iterator<?> iUnderlying) {
-        this.graph = graph;
-        this.underlying = iUnderlying;
+    Set<String> autoIndexKeys = null;
+    private static final String KEYS = "keys";
+
+    public OrientAutomaticIndex(final OrientGraph graph, final String indexName, final Class<T> indexClass, Set<String> indexKeys) {
+        super(graph, indexName, indexClass, Index.Type.AUTOMATIC);
+        if (indexKeys != null)
+            autoIndexKeys = new HashSet<String>(indexKeys);
+        init();
+        saveConfiguration();
     }
 
-    public boolean hasNext() {
-        return this.underlying.hasNext();
+    public OrientAutomaticIndex(OrientGraph graph, OIndex index) {
+        super(graph, index);
+        init();
     }
 
-    @SuppressWarnings("unchecked")
-    public T next() {
-        OrientElement currentElement=null;
-        final Object current = this.underlying.next();
-        if( current instanceof ODocument ){
-	        final ODocument currentDocument = (ODocument) current;
-	        if (null == currentDocument)
-	            throw new NoSuchElementException();
-	
-	        if (currentDocument.getInternalStatus() == STATUS.NOT_LOADED)
-	            currentDocument.load();
-	
-	        if (currentDocument.getSchemaClass().isSubClassOf(graph.getRawGraph().getEdgeBaseClass()))
-	            currentElement = new OrientEdge(graph, currentDocument);
-	        else
-	            currentElement = new OrientVertex(graph, currentDocument);
+    public Type getIndexType() {
+        return Type.AUTOMATIC;
+    }
+
+    protected void autoUpdate(final String key, final Object newValue, final Object oldValue, final T element) {
+        if (this.getIndexClass().isAssignableFrom(element.getClass()) && (this.autoIndexKeys == null || this.autoIndexKeys.contains(key))) {
+            if (oldValue != null)
+                this.remove(key, oldValue, element);
+            this.put(key, newValue, element);
         }
-        
-        return (T) currentElement;
     }
 
-    public void remove() {
-        throw new UnsupportedOperationException();
+    protected void autoRemove(final String key, final Object oldValue, final T element) {
+        if (this.getIndexClass().isAssignableFrom(element.getClass()) && (this.autoIndexKeys == null || this.autoIndexKeys.contains(key))) {
+            this.remove(key, oldValue, element);
+        }
     }
 
-    public Iterator<T> iterator() {
-        return this;
+    public Set<String> getAutoIndexKeys() {
+        return this.autoIndexKeys;
+    }
+
+    private void init() {
+        final Collection<Object> field = underlying.updateConfiguration().field(KEYS);
+        if (null != field) {
+            this.autoIndexKeys = new HashSet<String>();
+            for (Object key : field) {
+                this.autoIndexKeys.add((String) key);
+            }
+        }
+    }
+
+    private void saveConfiguration() {
+        underlying.getConfiguration().field(KEYS, this.autoIndexKeys);
+        graph.saveIndexConfiguration();
+    }
+
+    public String toString() {
+        return StringFactory.indexString(this);
     }
 }
