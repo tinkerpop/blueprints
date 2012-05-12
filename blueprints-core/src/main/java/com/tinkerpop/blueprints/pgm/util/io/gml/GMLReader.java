182cb82fea221112989740f92273870298f33d27
stephen mallette <spmallette@gmail.com>
2012-05-12 09:48:46 -0400

    Refactored GMLReader to be more like the other io implementations

--- blueprints-core/src/main/java/com/tinkerpop/blueprints/pgm/util/io/gml/GMLReader.java
+++ blueprints-core/src/main/java/com/tinkerpop/blueprints/pgm/util/io/gml/GMLReader.java
@@ -28,6 +28,7 @@ import java.util.Map.Entry;
  * also assume that only one graph can be defined in a file.
  *
  * @author Stuart Hendren (http://stuarthendren.net)
+ * @author Stephen Mallette
  */
 public class GMLReader {
 
@@ -35,15 +36,9 @@ public class GMLReader {
 
     private static final int DEFAULT_BUFFER_SIZE = 1000;
 
-    private Map<Object, Object> vertexIds = new HashMap<Object, Object>();
-
     private Graph graph;
 
     private final String defaultEdgeLabel;
-
-    private boolean directed = false;
-
-    private int edgeCount = 0;
 
     private String vertexIdKey;
 
@@ -103,7 +98,8 @@ public class GMLReader {
      * @throws IOException
      */
     public void inputGraph(InputStream inputStream) throws IOException {
-        inputGraph(inputStream, DEFAULT_BUFFER_SIZE);
+        GMLReader.inputGraph(this.graph, inputStream, DEFAULT_BUFFER_SIZE, this.defaultEdgeLabel,
+                this.vertexIdKey, this.edgeIdKey, this.edgeLabelKey);
     }
 
     /**
@@ -115,251 +111,8 @@ public class GMLReader {
      * @throws IOException
      */
     public void inputGraph(InputStream inputStream, int bufferSize) throws IOException {
-
-
-        if (graph instanceof TransactionalGraph) {
-            this.graph = new BufferGraph((TransactionalGraph) this.graph, bufferSize);
-        }
-
-        Reader r = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("ISO-8859-1")));
-        StreamTokenizer st = new StreamTokenizer(r);
-
-        try {
-            st.commentChar(GMLTokens.COMMENT_CHAR);
-            st.ordinaryChar('[');
-            st.ordinaryChar(']');
-
-            String stringCharacters = "/\\(){}<>!£$%^&*-+=,.?:;@_`|~";
-            for (int i = 0; i < stringCharacters.length(); i++) {
-                st.wordChars(stringCharacters.charAt(i), stringCharacters.charAt(i));
-            }
-
-            parse(st);
-
-            if (graph instanceof TransactionalGraph) {
-                ((TransactionalGraph) graph).stopTransaction(TransactionalGraph.Conclusion.SUCCESS);
-            }
-        } catch (IOException e) {
-            throw new IOException(error(st), e);
-        } finally {
-            vertexIds.clear();
-        }
-
-    }
-
-    private boolean hasNext(StreamTokenizer st) throws IOException {
-        return st.nextToken() != StreamTokenizer.TT_EOF;
-    }
-
-    private String error(StreamTokenizer st) {
-        return "GML malformed line number " + st.lineno() + ": ";
-    }
-
-    private boolean notLineBreak(int type) {
-        return type != StreamTokenizer.TT_EOL;
-    }
-
-    private void parse(StreamTokenizer st) throws IOException {
-        while (hasNext(st)) {
-            int type = st.ttype;
-            if (notLineBreak(type)) {
-                String value = st.sval;
-                if (GMLTokens.GRAPH.equals(value)) {
-                    parseGraph(st);
-                    if (!hasNext(st)) {
-                        return;
-                    }
-                }
-            }
-        }
-        throw new IOException("Graph not complete");
-    }
-
-    private void parseGraph(StreamTokenizer st) throws IOException {
-        checkValid(st, GMLTokens.GRAPH);
-        while (hasNext(st)) {
-            // st.nextToken();
-            int type = st.ttype;
-            if (notLineBreak(type)) {
-                if (type == ']') {
-                    return;
-                } else {
-                    String key = st.sval;
-                    if (GMLTokens.NODE.equals(key)) {
-                        addNode(parseNode(st));
-                    } else if (GMLTokens.EDGE.equals(key)) {
-                        addEdge(parseEdge(st));
-                    } else if (GMLTokens.DIRECTED.equals(key)) {
-                        directed = parseBoolean(st);
-                    } else {
-                        // IGNORE
-                        parseValue("ignore", st);
-                    }
-                }
-            }
-        }
-        throw new IOException("Graph not complete");
-    }
-
-    private void addNode(Map<String, Object> map) throws IOException {
-        Object id = map.remove(GMLTokens.ID);
-        if (id != null) {
-            Vertex vertex;
-            vertex = createVertex(map, id);
-            addProperties(vertex, map);
-        } else {
-            throw new IOException("No id found for node");
-        }
-    }
-
-    private Vertex createVertex(Map<String, Object> map, Object id) {
-        if (vertexIdKey != null) {
-            Object vertexId = map.remove(vertexIdKey);
-            if (vertexId != null) {
-                vertexIds.put(id, vertexId);
-                return graph.addVertex(vertexId);
-            }
-            // if no id try to use default id this will fail if not unique
-            vertexIds.put(id, id);
-        }
-        // Use default id system
-        return graph.addVertex(id);
-    }
-
-    private void addEdge(Map<String, Object> map) throws IOException {
-        Object source = map.remove(GMLTokens.SOURCE);
-        Object target = map.remove(GMLTokens.TARGET);
-        if (source == null) {
-            throw new IOException("Edge has no source");
-        }
-        if (target == null) {
-            throw new IOException("Edge has no target");
-        }
-        if (vertexIdKey != null) {
-            source = vertexIds.get(source);
-            target = vertexIds.get(target);
-        }
-
-        Vertex outVertex = graph.getVertex(source);
-        Vertex inVertex = graph.getVertex(target);
-        if (outVertex == null) {
-            throw new IOException("Edge source " + source + " not found");
-        }
-        if (inVertex == null) {
-            throw new IOException("Edge target " + target + " not found");
-
-        }
-
-        Object label = map.remove(edgeLabelKey);
-        if (label == null) {
-            // try standard label key
-            label = map.remove(GMLTokens.LABEL);
-        } else {
-            // remove label in case edge label key is not label
-            // label is reserved and cannot be added as a property
-            // if so this data will be lost
-            map.remove(GMLTokens.LABEL);
-        }
-
-        if (label == null) {
-            label = defaultEdgeLabel;
-        }
-        Object edgeId = edgeCount++;
-        if (edgeIdKey != null) {
-            Object mappedKey = map.remove(edgeIdKey);
-            if (mappedKey != null) {
-                edgeId = mappedKey;
-            }
-            // else use edgecount - could fail if mapped ids overlap with edge count
-        }
-
-        // remove id as reserved property - can be left is edgeIdKey in not id
-        // This data will be lost
-        map.remove(GMLTokens.ID);
-
-        Edge edge = graph.addEdge(edgeId, outVertex, inVertex, label.toString());
-        if (directed) {
-            edge.setProperty(GMLTokens.DIRECTED, directed);
-        }
-        addProperties(edge, map);
-
-    }
-
-    private void addProperties(Element element, Map<String, Object> map) {
-        for (Entry<String, Object> entry : map.entrySet()) {
-            element.setProperty(entry.getKey(), entry.getValue());
-        }
-    }
-
-    private Object parseValue(String key, StreamTokenizer st) throws IOException {
-        while (hasNext(st)) {
-            int type = st.ttype;
-            if (notLineBreak(type)) {
-                if (type == StreamTokenizer.TT_NUMBER) {
-                    Double doubleValue = Double.valueOf(st.nval);
-                    if (doubleValue.equals(Double.valueOf(doubleValue.intValue()))) {
-                        return doubleValue.intValue();
-                    } else {
-                        return doubleValue.floatValue();
-                    }
-                } else {
-                    if (type == '[') {
-                        return parseMap(key, st);
-                    } else if (type == '"') {
-                        return st.sval;
-                    }
-                }
-            }
-        }
-        throw new IOException("value not found");
-    }
-
-    private boolean parseBoolean(StreamTokenizer st) throws IOException {
-        while (hasNext(st)) {
-            int type = st.ttype;
-            if (notLineBreak(type)) {
-                if (type == StreamTokenizer.TT_NUMBER) {
-                    return st.nval == 1.0;
-                }
-            }
-        }
-        throw new IOException("boolean not found");
-    }
-
-    private Map<String, Object> parseNode(StreamTokenizer st) throws IOException {
-        return parseElement(st, GMLTokens.NODE);
-    }
-
-    private Map<String, Object> parseEdge(StreamTokenizer st) throws IOException {
-        return parseElement(st, GMLTokens.EDGE);
-    }
-
-    private Map<String, Object> parseElement(StreamTokenizer st, String node) throws IOException {
-        checkValid(st, node);
-        return parseMap(node, st);
-    }
-
-    private Map<String, Object> parseMap(String node, StreamTokenizer st) throws IOException {
-        Map<String, Object> map = new HashMap<String, Object>();
-        while (hasNext(st)) {
-            int type = st.ttype;
-            if (notLineBreak(type)) {
-                if (type == ']') {
-                    return map;
-                } else {
-                    String key = st.sval;
-                    Object value = parseValue(key, st);
-                    map.put(key, value);
-                }
-            }
-        }
-        throw new IOException(node + " incomplete");
-    }
-
-    private void checkValid(StreamTokenizer st, String token) throws IOException {
-        if (st.nextToken() != '[') {
-            throw new IOException(token + " not followed by [");
-        }
+        GMLReader.inputGraph(this.graph, inputStream, bufferSize, this.defaultEdgeLabel,
+                this.vertexIdKey, this.edgeIdKey, this.edgeLabelKey);
     }
 
     /**
@@ -370,18 +123,46 @@ public class GMLReader {
      * @throws IOException thrown if the data is not valid
      */
     public static void inputGraph(Graph graph, InputStream inputStream) throws IOException {
-        inputGraph(graph, inputStream, DEFAULT_LABEL);
+        inputGraph(graph, inputStream, DEFAULT_BUFFER_SIZE, DEFAULT_LABEL, null, null, null);
     }
 
     /**
      * Load the GML file into the Graph.
      *
-     * @param graph            to receive the data
+     * @param inputGraph       to receive the data
      * @param inputStream      GML file
      * @param defaultEdgeLabel default edge label to be used if not defined in the data
+     * @param vertexIdKey      if the id of a vertex is a &lt;data/&gt; property, fetch it from the data property.
+     * @param edgeIdKey        if the id of an edge is a &lt;data/&gt; property, fetch it from the data property.
+     * @param edgeLabelKey     if the label of an edge is a &lt;data/&gt; property, fetch it from the data property.
      * @throws IOException thrown if the data is not valid
      */
-    public static void inputGraph(Graph graph, InputStream inputStream, String defaultEdgeLabel) throws IOException {
-        new GMLReader(graph, defaultEdgeLabel).inputGraph(inputStream, DEFAULT_BUFFER_SIZE);
+    public static void inputGraph(final Graph inputGraph, final InputStream inputStream, final int bufferSize, 
+                                  final String defaultEdgeLabel, final String vertexIdKey, final String edgeIdKey, 
+                                  final String edgeLabelKey) throws IOException {
+        final Graph graph = inputGraph instanceof TransactionalGraph ?
+                new BufferGraph((TransactionalGraph) inputGraph, bufferSize) : inputGraph;
+
+        final Reader r = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("ISO-8859-1")));
+        final StreamTokenizer st = new StreamTokenizer(r);
+
+        try {
+            st.commentChar(GMLTokens.COMMENT_CHAR);
+            st.ordinaryChar('[');
+            st.ordinaryChar(']');
+
+            final String stringCharacters = "/\\(){}<>!£$%^&*-+=,.?:;@_`|~";
+            for (int i = 0; i < stringCharacters.length(); i++) {
+                st.wordChars(stringCharacters.charAt(i), stringCharacters.charAt(i));
+            }
+
+            new GMLParser(graph, defaultEdgeLabel, vertexIdKey, edgeIdKey, edgeLabelKey).parse(st);
+
+            if (graph instanceof TransactionalGraph) {
+                ((TransactionalGraph) graph).stopTransaction(TransactionalGraph.Conclusion.SUCCESS);
+            }
+        } catch (IOException e) {
+            throw new IOException("GML malformed line number " + st.lineno() + ": ", e);
+        } 
     }
 }
