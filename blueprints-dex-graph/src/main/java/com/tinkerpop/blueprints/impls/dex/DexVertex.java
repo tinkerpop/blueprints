ace2e9a0575d850ab8f1c04e3da976bfe0879707
Sergio Gomez-Villamor <sgomezvillamor@gmail.com>
2012-05-17 14:54:04 +0200

    DexVertex: (i) use MultiIterable instead of perform Objects unions (ii) use neighbors to implement getVertices

--- blueprints-dex-graph/src/main/java/com/tinkerpop/blueprints/impls/dex/DexVertex.java
+++ blueprints-dex-graph/src/main/java/com/tinkerpop/blueprints/impls/dex/DexVertex.java
@@ -10,7 +10,6 @@ import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.util.DefaultQuery;
 import com.tinkerpop.blueprints.util.MultiIterable;
 import com.tinkerpop.blueprints.util.StringFactory;
-import com.tinkerpop.blueprints.util.VerticesFromEdgesIterable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -44,31 +43,53 @@ public class DexVertex extends DexElement implements Vertex {
     }
 
     public Iterable<Vertex> getVertices(final Direction direction, final String... labels) {
-        return new VerticesFromEdgesIterable(this, direction, labels);
+        if (direction.equals(Direction.OUT)) {
+            return this.getOutVertices(labels);
+        } else if (direction.equals(Direction.IN))
+            return this.getInVertices(labels);
+        else {
+            return new MultiIterable<Vertex>(Arrays.asList(this.getInVertices(labels), this.getOutVertices(labels)));
+        }
     }
 
     private Iterable<Edge> getOutEdgesNoLabels() {
-        com.sparsity.dex.gdb.Objects result = graph.getRawSession().newObjects();
         com.sparsity.dex.gdb.TypeList tlist = graph.getRawGraph().findEdgeTypes();
+        final List<Iterable<Edge>> edges = new ArrayList<Iterable<Edge>>();
         for (Integer etype : tlist) {
-            com.sparsity.dex.gdb.Objects objs = graph.getRawGraph().explode(oid, etype, com.sparsity.dex.gdb.EdgesDirection.Outgoing);
-            result.union(objs);
-            objs.close();
+        	edges.add(getOutEdgesSingleType(etype));
         }
         tlist = null;
-        return new DexIterable<Edge>(graph, result, Edge.class);
+        return new MultiIterable<Edge>(edges);
+    }
+
+    private Iterable<Vertex> getOutVerticesNoLabels() {
+        com.sparsity.dex.gdb.TypeList tlist = graph.getRawGraph().findEdgeTypes();
+        final List<Iterable<Vertex>> vertices = new ArrayList<Iterable<Vertex>>();
+        for (Integer etype : tlist) {
+        	vertices.add(getOutVerticesSingleType(etype));
+        }
+        tlist = null;
+        return new MultiIterable<Vertex>(vertices);
     }
 
     private Iterable<Edge> getInEdgesNoLabels() {
-        com.sparsity.dex.gdb.Objects result = graph.getRawSession().newObjects();
         com.sparsity.dex.gdb.TypeList tlist = graph.getRawGraph().findEdgeTypes();
+        final List<Iterable<Edge>> edges = new ArrayList<Iterable<Edge>>();
         for (Integer etype : tlist) {
-            com.sparsity.dex.gdb.Objects objs = graph.getRawGraph().explode(oid, etype, com.sparsity.dex.gdb.EdgesDirection.Ingoing);
-            result.union(objs);
-            objs.close();
+        	edges.add(getInEdgesSingleType(etype));
         }
         tlist = null;
-        return new DexIterable<Edge>(graph, result, Edge.class);
+        return new MultiIterable<Edge>(edges);
+    }
+
+    private Iterable<Vertex> getInVerticesNoLabels() {
+        com.sparsity.dex.gdb.TypeList tlist = graph.getRawGraph().findEdgeTypes();
+        final List<Iterable<Vertex>> vertices = new ArrayList<Iterable<Vertex>>();
+        for (Integer etype : tlist) {
+        	vertices.add(getInVerticesSingleType(etype));
+        }
+        tlist = null;
+        return new MultiIterable<Vertex>(vertices);
     }
 
     private Iterable<Edge> getOutEdgesSingleLabel(final String label) {
@@ -77,8 +98,26 @@ public class DexVertex extends DexElement implements Vertex {
             return new ArrayList<Edge>();
         }
 
+        return getOutEdgesSingleType(type);
+    }
+
+    private Iterable<Vertex> getOutVerticesSingleLabel(final String label) {
+        int type = DexTypes.getTypeId(graph.getRawGraph(), label);
+        if (type == com.sparsity.dex.gdb.Type.InvalidType) {
+            return new ArrayList<Vertex>();
+        }
+
+        return getOutVerticesSingleType(type);
+    }
+
+    private Iterable<Edge> getOutEdgesSingleType(final int type) {
         com.sparsity.dex.gdb.Objects objs = graph.getRawGraph().explode(oid, type, com.sparsity.dex.gdb.EdgesDirection.Outgoing);
         return new DexIterable<Edge>(graph, objs, Edge.class);
+    }
+
+    private Iterable<Vertex> getOutVerticesSingleType(final int type) {
+        com.sparsity.dex.gdb.Objects objs = graph.getRawGraph().neighbors(oid, type, com.sparsity.dex.gdb.EdgesDirection.Outgoing);
+        return new DexIterable<Vertex>(graph, objs, Vertex.class);
     }
 
     private Iterable<Edge> getInEdgesSingleLabel(final String label) {
@@ -87,8 +126,26 @@ public class DexVertex extends DexElement implements Vertex {
             return new ArrayList<Edge>();
         }
 
+        return getInEdgesSingleType(type);
+    }
+
+    private Iterable<Vertex> getInVerticesSingleLabel(final String label) {
+        int type = DexTypes.getTypeId(graph.getRawGraph(), label);
+        if (type == com.sparsity.dex.gdb.Type.InvalidType) {
+            return new ArrayList<Vertex>();
+        }
+
+        return getInVerticesSingleType(type);
+    }
+
+    private Iterable<Edge> getInEdgesSingleType(final int type) {
         com.sparsity.dex.gdb.Objects objs = graph.getRawGraph().explode(oid, type, com.sparsity.dex.gdb.EdgesDirection.Ingoing);
         return new DexIterable<Edge>(graph, objs, Edge.class);
+    }
+
+    private Iterable<Vertex> getInVerticesSingleType(final int type) {
+        com.sparsity.dex.gdb.Objects objs = graph.getRawGraph().neighbors(oid, type, com.sparsity.dex.gdb.EdgesDirection.Ingoing);
+        return new DexIterable<Vertex>(graph, objs, Vertex.class);
     }
 
     public String toString() {
@@ -109,6 +166,20 @@ public class DexVertex extends DexElement implements Vertex {
         }
     }
 
+    private Iterable<Vertex> getInVertices(final String... labels) {
+        if (labels.length == 0)
+            return this.getInVerticesNoLabels();
+        else if (labels.length == 1) {
+            return this.getInVerticesSingleLabel(labels[0]);
+        } else {
+            final List<Iterable<Vertex>> vertices = new ArrayList<Iterable<Vertex>>();
+            for (final String label : labels) {
+            	vertices.add(this.getInVerticesSingleLabel(label));
+            }
+            return new MultiIterable<Vertex>(vertices);
+        }
+    }
+
     private Iterable<Edge> getOutEdges(final String... labels) {
         if (labels.length == 0)
             return this.getOutEdgesNoLabels();
@@ -120,6 +191,20 @@ public class DexVertex extends DexElement implements Vertex {
                 edges.add(this.getOutEdgesSingleLabel(label));
             }
             return new MultiIterable<Edge>(edges);
+        }
+    }
+
+    private Iterable<Vertex> getOutVertices(final String... labels) {
+        if (labels.length == 0)
+            return this.getOutVerticesNoLabels();
+        else if (labels.length == 1) {
+            return this.getOutVerticesSingleLabel(labels[0]);
+        } else {
+            final List<Iterable<Vertex>> vertices = new ArrayList<Iterable<Vertex>>();
+            for (final String label : labels) {
+            	vertices.add(this.getOutVerticesSingleLabel(label));
+            }
+            return new MultiIterable<Vertex>(vertices);
         }
     }
 
