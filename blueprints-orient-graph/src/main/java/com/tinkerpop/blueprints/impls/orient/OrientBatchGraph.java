0ab461fd624bcfa4806855e1d6256b765ee808ff
Stephen Mallette <spmva@genoprime.com>
2012-10-11 05:57:31 -0400

    Bump to OrientDB 1.2.
    Fixes problem with vertex duplicates #312.

--- blueprints-orient-graph/src/main/java/com/tinkerpop/blueprints/impls/orient/OrientBatchGraph.java
+++ blueprints-orient-graph/src/main/java/com/tinkerpop/blueprints/impls/orient/OrientBatchGraph.java
@@ -55,17 +55,14 @@ public class OrientBatchGraph extends OrientBaseGraph {
      */
     public OrientBatchGraph(final OGraphDatabase iDatabase) {
         super(iDatabase);
-        getRawGraph().declareIntent( new OIntentMassiveInsert() );
     }
 
     public OrientBatchGraph(final String url) {
         super(url, ADMIN, ADMIN);
-        getRawGraph().declareIntent( new OIntentMassiveInsert() );
     }
 
     public OrientBatchGraph(final String url, final String username, final String password) {
         super(url, username, password);
-        getRawGraph().declareIntent( new OIntentMassiveInsert() );
     }
 
     public Features getFeatures() {
