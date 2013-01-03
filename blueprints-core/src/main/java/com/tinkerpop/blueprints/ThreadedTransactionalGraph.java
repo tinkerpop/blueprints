ce58c168b5567569af309bcfa7e0182a03dad00c
okram <okrammarko@gmail.com>
2013-01-03 08:58:42 -0700

    deprecated startTransaction() and called it newTransaction() in ThreadedTransactionalGraph.

--- blueprints-core/src/main/java/com/tinkerpop/blueprints/ThreadedTransactionalGraph.java
+++ blueprints-core/src/main/java/com/tinkerpop/blueprints/ThreadedTransactionalGraph.java
@@ -2,21 +2,27 @@ package com.tinkerpop.blueprints;
 
 /**
  * ThreadedTransactionalGraph provides more fine grained control over the transactional context.
- * While TransactionalGraph binds each transaction to the executing thread, ThreadedTransactionalGraph's {@link #startTransaction} returns a {@link TransactionalGraph} that represents its own transactional context independent of the executing thread.
+ * While TransactionalGraph binds each transaction to the executing thread, ThreadedTransactionalGraph's {@link #newTransaction} returns a {@link TransactionalGraph} that represents its own transactional context independent of the executing thread.
  * Hence, one can have multiple threads operating against a single transaction represented by the returned TransactionalGraph object. This is useful for parallelizing graph algorithms.
  * <p/>
  * Note, that one needs to call {@link TransactionalGraph#commit()} or {@link TransactionalGraph#rollback()} to close the transactions returned
- * by {@link #startTransaction()}.
+ * by {@link #newTransaction()}.
  *
  * @author Matthias Brocheler (http://matthiasb.com)
  */
 public interface ThreadedTransactionalGraph extends TransactionalGraph {
+
+    @Deprecated
+    /**
+     * Please use newTransaction() as this method has been renamed for clarity.
+     */
+    public TransactionalGraph startTransaction();
 
     /**
      * Returns a {@link TransactionalGraph} that represents a transactional context independent of the executing transaction.
      *
      * @return A transactional context. Invoking TransactionalGraph.shutdown() successfully commits the transaction.
      */
-    public TransactionalGraph startTransaction();
+    public TransactionalGraph newTransaction();
 
 }
