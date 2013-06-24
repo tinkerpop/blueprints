a8963eb699956ed9ab667b062c0dcde37f87cce5
oreissig <oreissig+github@gmail.com>
2013-06-24 12:04:35 +0200

    use more generic java.lang.AutoCloseable instead of I/O related java.io.Closeable
    Actually it should implement java.lang.AutoCloseable, as java.io.Closeable is only meant for I/O related stuff (like Streams) and therefore throws IOException (which is not appropriate for Graphs). AutoCloseable also supports all the fancy syntactic sugar, just without the I/O focus.

--- blueprints-core/src/main/java/com/tinkerpop/blueprints/CloseableIterable.java
+++ blueprints-core/src/main/java/com/tinkerpop/blueprints/CloseableIterable.java
@@ -1,13 +1,11 @@
 package com.tinkerpop.blueprints;
 
-import java.io.Closeable;
-
 /**
  * A CloseableIterable is required where it is necessary to deallocate resources from an Iterable.
  *
  * @author Marko A. Rodriguez (http://markorodriguez.com)
  */
-public interface CloseableIterable<T> extends Iterable<T>, Closeable {
+public interface CloseableIterable<T> extends Iterable<T>, AutoCloseable {
 
     /**
      * Release the resources of the iterator.
