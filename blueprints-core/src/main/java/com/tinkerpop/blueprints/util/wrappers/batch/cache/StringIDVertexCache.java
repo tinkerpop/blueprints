7473a6f820d0a9c3894a6f49fd1bcdbe9bf9f3af
Dan LaRocque <dalaro@hopcount.org>
2013-07-03 23:34:11 -0700

    Refactored and optimized batch vertex caches impls
    This commit pulls common batch-loading vertex cache code into
    AbstractIDVertexCache and makes String and ObjectIDVertexCache extend
    that abstract base class.  LongIDVertexCache uses Colt primitive
    collections internally and is not amenable to sharing code with the
    other Java collections-based implementations.
    
    This commit also applies the optimization from
    a0cf0f11d8c4eaf436493f742789142d741ac06e to Object and
    LongIDVertexCache.  However, I've only tested it for
    StringIDVertexCache.

--- blueprints-core/src/main/java/com/tinkerpop/blueprints/util/wrappers/batch/cache/StringIDVertexCache.java
+++ blueprints-core/src/main/java/com/tinkerpop/blueprints/util/wrappers/batch/cache/StringIDVertexCache.java
@@ -1,31 +1,20 @@
 package com.tinkerpop.blueprints.util.wrappers.batch.cache;
 
-import com.tinkerpop.blueprints.Graph;
 import com.tinkerpop.blueprints.Vertex;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * (c) Matthias Broecheler (me@matthiasb.com)
  */
 
-public class StringIDVertexCache implements VertexCache {
+public class StringIDVertexCache extends AbstractIDVertexCache {
 
-    private static final int INITIAL_CAPACITY = 1000;
-    private static final int INITIAL_TX_CAPACITY = 100;
-
-    private final Map<String, Object> map;
-    private final Set<String> mapKeysInCurrentTx;
     private final StringCompression compression;
 
     public StringIDVertexCache(final StringCompression compression) {
+        super();
         if (compression == null) throw new IllegalArgumentException("Compression expected.");
         this.compression = compression;
-        map = new HashMap<String, Object>(INITIAL_CAPACITY);
-        mapKeysInCurrentTx = new HashSet<String>(INITIAL_TX_CAPACITY);
     }
 
     public StringIDVertexCache() {
@@ -34,37 +23,16 @@ public class StringIDVertexCache implements VertexCache {
 
     @Override
     public Object getEntry(Object externalId) {
-        String id = compression.compress(externalId.toString());
-        return map.get(id);
-    }
-
-    @Override
-    public void set(Vertex vertex, Object externalId) {
-        setId(vertex,externalId);
+        return super.getEntry(compression.compress(externalId.toString()));
     }
 
     @Override
     public void setId(Object vertexId, Object externalId) {
-        String id = compression.compress(externalId.toString());
-        map.put(id, vertexId);
-        mapKeysInCurrentTx.add(id);
+        super.setId(vertexId, compression.compress(externalId.toString()));
     }
 
     @Override
     public boolean contains(Object externalId) {
-        return map.containsKey(compression.compress(externalId.toString()));
-    }
-
-    @Override
-    public void newTransaction() {
-        for (String id : mapKeysInCurrentTx) {
-            Object o = map.get(id);
-            assert null != o;
-            if (o instanceof Vertex) {
-                Vertex v = (Vertex)o;
-                map.put(id, v.getId());
-            }
-        }
-        mapKeysInCurrentTx.clear();
+        return super.contains(compression.compress(externalId.toString()));
     }
 }
