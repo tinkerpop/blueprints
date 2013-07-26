1eb5641718ef30766be437bc11a0bbaafc19d8cc
lvca <l.garulli@gmail.com>
2013-07-26 15:54:53 +0200

    Fixed OrientDB issue #1571 about a bug on seting properties against loaded edges

--- blueprints-orient-graph/src/main/java/com/tinkerpop/blueprints/impls/orient/OrientElement.java
+++ blueprints-orient-graph/src/main/java/com/tinkerpop/blueprints/impls/orient/OrientElement.java
@@ -160,7 +160,7 @@ public abstract class OrientElement implements Element, OSerializableStream, OId
             return null;
 
         // CHANGE THE RID -> DOCUMENT
-        //rawElement = doc;
+        rawElement = doc;
         return doc;
     }
 
