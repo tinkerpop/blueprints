fc8a5ef4d01c5664c1b75c46155aaef4ae000a24
Marko A. Rodriguez <okrammarko@gmail.com>
2013-09-28 13:22:38 -0600

    commit transaction after indexing.

--- blueprints-core/src/main/java/com/tinkerpop/blueprints/util/KeyIndexableGraphHelper.java
+++ blueprints-core/src/main/java/com/tinkerpop/blueprints/util/KeyIndexableGraphHelper.java
@@ -38,6 +38,9 @@ public class KeyIndexableGraphHelper {
                 }
             }
         }
+        if (isTransactional) {
+            ((TransactionalGraph) graph).commit();
+        }
         return counter;
     }
 }
