69661db5e857f94dfaa61f7d72669c472c252c2d
Stephen Mallette <spmva@genoprime.com>
2013-11-27 05:22:26 -0500

    Manually convert apache Configuration object to Map for Neo4jHA.
    The apache converter was being a little too smart and convering comma separated property values to List.

--- blueprints-neo4j-graph/src/main/java/com/tinkerpop/blueprints/impls/neo4j/Neo4jHaGraph.java
+++ blueprints-neo4j-graph/src/main/java/com/tinkerpop/blueprints/impls/neo4j/Neo4jHaGraph.java
@@ -5,6 +5,8 @@ import org.apache.commons.configuration.ConfigurationConverter;
 import org.neo4j.graphdb.factory.HighlyAvailableGraphDatabaseFactory;
 import org.neo4j.kernel.ha.HighlyAvailableGraphDatabase;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -28,6 +30,16 @@ public class Neo4jHaGraph extends Neo4jGraph {
 
     public Neo4jHaGraph(final Configuration configuration) {
         this(configuration.getString("blueprints.neo4jha.directory", null),
-                ConfigurationConverter.getMap(configuration.subset("blueprints.neo4jha.conf")));
+                convertConfiguration(configuration.subset("blueprints.neo4jha.conf")));
+    }
+
+    private static Map<String,String> convertConfiguration(final Configuration configuration) {
+        final Map<String,String> c = new HashMap<String,String>();
+        final Iterator<String> keys = configuration.getKeys();
+        while (keys.hasNext()) {
+            final String k = keys.next();
+            c.put(k, configuration.getString(k));
+        }
+        return c;
     }
 }
