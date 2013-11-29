56339f02d8e17635aea2804635b359cb515ed901
xsanchez <xsanchez@sparsity-technologies.com>
2013-11-29 13:20:50 +0100

    Removal of blueprints-dex-graph

--- blueprints-dex-graph/src/main/java/com/tinkerpop/blueprints/impls/dex/DexTokens.java
+++
@@ -1,9 +0,0 @@
-package com.tinkerpop.blueprints.impls.dex;
-
-/**
- * @author Marko A. Rodriguez (http://markorodriguez.com)
- */
-class DexTokens {
-
-    public static final String TYPE_EXCEPTION_MESSAGE = "Boolean, integer, long, string, floats, and doubles are the only supported property value types";
-}
